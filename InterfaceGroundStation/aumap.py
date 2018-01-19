bc584064ca9f56cd444eee50638f8ec406d85410
EstebanStruve <estebanjavierst@gmail.com>
2018-01-19 13:44:26 +0100

    Add cursor fixed, added reset button
    Added the cursor to save location, mouse out of map doesn't move markers, added reset button in auto mode.

--- InterfaceGroundStation/aumap.py
+++ InterfaceGroundStation/aumap.py
@@ -45,7 +45,7 @@ mapsize = width, height = 850,850
 screensize = 1500,850
 screen = pygame.display.set_mode(screensize)
 #Setting margins in screen
-margtextx,margtexty = width+220,80
+margtextx,margtexty = width+170,80
 margswitchx,margswitchy = width+40,140
 margmodex,margmodey = width+50,(height/2)+100
 margsw1x,margsw1y = margmodex+110,margmodey+40
@@ -56,6 +56,8 @@ ind1=0
 ind2=0
 coordx,coordy=0,0
 mousex,mousey=0,0
+mxdisp,mydisp=0,0
+mxfixed,myfixed=0,0
 movex, movey=0,0
 endx,endy=0,0
 opx,opy=0,0
@@ -68,12 +70,25 @@ timerdisp=0
 #Fonts for text
 myfont=pygame.font.SysFont("Arial Black",20)
 myfont2=pygame.font.Font(None,20)
+#Create and load text for right side of screen
+texttit1=myfont.render("Angle values:",0,ORANGE)
+texttit2=myfont.render("Position of cursor:",0,ORANGE)
+texttit3=myfont.render("Time:",0,ORANGE)
+textbut1=myfont.render("Play",0,BLACK)
+textbut2=myfont.render("Playing",0,BLACK)
+textbut3=myfont.render("Stop",0,BLACK)
+textbut4=myfont.render("Reset",0,BLACK)
+textmanual1=myfont.render("Use the arrow keys",0,GREY1)
+textmanual2=myfont.render("to navigate de values",0,GREY1)
+textcursor1=myfont.render("Right-clik map to save location",0,GREY1)
+textcursor2=myfont.render("Right-clik map to drop location",0,GREY1)
 #Start clock
 clock=pygame.time.Clock()
 #Booleans for states
 boolmode=True
 boolzoom=True
 timerrun=False
+boolcursor=True
 """*************************Pygame While Loop******************************"""
 while True:
     if timerrun == True:
@@ -120,9 +135,21 @@ while True:
         mousey=height
     elif mousey < 0:
         mousey=0
+    #Update the displayed coordinates only when cursor inside the map
+    if 0 < mousex < width and 0 < mousey < height:
+        if clicked[2]==0:
+            mxdisp=mousex
+            mydisp=mousey
+        else:
+            mxfixed=mousex
+            myfixed=mousey
+            if boolcursor==True:
+                boolcursor=False
+            else:
+                boolcursor=True
     #coordinates for cursor arrow to allign to arrow tip
-    arrowx=mousex-10
-    arrowy=mousey-20
+    arrowx=mxdisp-10
+    arrowy=mydisp-20
     #Choose an angle and slope from the list created by the csv file
     angledeg=azangles[ind1]
     slope=slopel[ind1]
@@ -211,9 +238,9 @@ while True:
     #Loading image objects
     pygame.display.set_caption("Ground Station")
     if boolzoom == True:
-        layer = pygame.image.load("mapaarhusn.png")
+        layer = pygame.image.load("mapin.png")
     else:
-        layer = pygame.image.load("mapaarhus.png")
+        layer = pygame.image.load("mapout.png")
     cursor=pygame.image.load("arrow.png")
     margmarkx1=pygame.image.load("marginmarkerx1.png")
     margmarkx2=pygame.image.load("marginmarkerx2.png")
@@ -226,34 +253,35 @@ while True:
     switch4=pygame.image.load("switch4.png")
     """*************************UPDATE SCREEN*******************************"""
     pygame.display.update()
+    #Update text
+    textang1=myfont.render("Azimut: "+str(angledeg)+" degrees",0,GREEN)
+    textang2=myfont.render("Elevation: "+str(elevang)+" degrees",0,BLUE2)
+    if boolcursor==True:
+        textcursor=myfont.render("X: "+str(mxdisp)+" Y: "+str(mydisp),0,YELLOW)
+    else:
+        textcursor=myfont.render("X: "+str(mxfixed)+" Y: "+str(myfixed)+" fixed",0,YELLOW)
+    texttime=myfont.render(str(timerdisp)+" seconds",0,YELLOW)
     #Black background
     screen.fill(GREY2)
     #Draw map
     screen.blit(layer,(0,0))
     #Draw information boxes
-    pygame.draw.rect(screen,GREY3,(margtextx-100,margtexty-50,450,450))
+    pygame.draw.rect(screen,GREY3,(margtextx-50,margtexty-50,450,450))
     pygame.draw.rect(screen,GREY3,(margmodex,margmodey,520,200))
     #Draw the lines
     pygame.draw.line(screen,YELLOW,(width/2,height/2),(opx,opy),3)
     pygame.draw.line(screen,GREEN,(width/2,height/2),(endx,endy),3)
-    #Create text right side of screen
-    texttit1=myfont.render("Angle values:",0,ORANGE)
-    textang1=myfont.render("Azimut: "+str(angledeg)+" degrees",0,GREEN)
-    textang2=myfont.render("Elevation: "+str(elevang)+" degrees",0,BLUE2)
-    texttit2=myfont.render("Position of cursor:",0,ORANGE)
-    texttit3=myfont.render("Time:",0,ORANGE)
-    textcursor=myfont.render("X: "+str(mousex)+" Y: "+str(mousey),0,YELLOW)
-    texttime=myfont.render(str(timerdisp)+" seconds",0,YELLOW)
-    textbut1=myfont.render("Play",0,BLACK)
-    textbut2=myfont.render("Playing",0,BLACK)
-    textbut3=myfont.render("Stop",0,BLACK)
-    textmanual1=myfont.render("Use the arrow keys",0,BLACK)
-    textmanual2=myfont.render("to navigate de values",0,BLACK)
     #Draw cursor margin markers
-    screen.blit(margmarkx1,(0,mousey-11))
-    screen.blit(margmarkx2,(width-30,mousey-11))
-    screen.blit(margmarky1,(mousex-11,0))
-    screen.blit(margmarky2,(mousex-11,height-30))
+    if boolcursor==True:
+        screen.blit(margmarkx1,(0,mydisp-11))
+        screen.blit(margmarkx2,(width-30,mydisp-11))
+        screen.blit(margmarky1,(mxdisp-11,0))
+        screen.blit(margmarky2,(mxdisp-11,height-30))
+    else:
+        screen.blit(margmarkx1,(0,myfixed-11))
+        screen.blit(margmarkx2,(width-30,myfixed-11))
+        screen.blit(margmarky1,(mxfixed-11,0))
+        screen.blit(margmarky2,(mxfixed-11,height-30))
     #Create angle value inside circle
     textcirc=myfont2.render(str(int(elevang)),0,BLUE2)
     textcircshad=myfont2.render(str(int(elevang)),0,BLACK)
@@ -263,8 +291,12 @@ while True:
     screen.blit(textang2,(margtextx,margtexty+80))
     screen.blit(texttit2,(margtextx,margtexty+130))
     screen.blit(textcursor,(margtextx,margtexty+170))
-    screen.blit(texttit3,(margtextx,margtexty+220))
-    screen.blit(texttime,(margtextx,margtexty+260))
+    if boolcursor==True:
+        screen.blit(textcursor1,(margtextx,margtexty+220))
+    else:
+        screen.blit(textcursor2,(margtextx,margtexty+220))
+    screen.blit(texttit3,(margtextx,margtexty+260))
+    screen.blit(texttime,(margtextx,margtexty+300))
     #Draw the switches
     #Configuration of switch 1 "Change Mode"
     if margsw1x < globmx < margsw1x+150 and margsw1y < globmy < margsw1y+50:
@@ -291,27 +323,41 @@ while True:
     #Draw buttons on the right side of screen
     #Configuration for button 1
     if boolmode == False: #In Auto Mode
-        pygame.draw.rect(screen,GREEN,(margmodex+130,margmodey+110,100,50))
-        if margmodex+130 < globmx < margmodex+230 and margmodey+110 < globmy < margmodey+160:
+        pygame.draw.rect(screen,GREEN,(margmodex+110,margmodey+110,100,50))
+        if margmodex+110 < globmx < margmodex+210 and margmodey+110 < globmy < margmodey+160:
             if clicked[0] == 1:
                 timerrun=True
         if timerrun == False:
-            screen.blit(textbut1,(margmodex+140,margmodey+120))
+            screen.blit(textbut1,(margmodex+120,margmodey+120))
         else:
-            pygame.draw.rect(screen,YELLOW,(margmodex+130,margmodey+110,100,50))
-            screen.blit(textbut2,(margmodex+140,margmodey+120))
+            pygame.draw.rect(screen,YELLOW,(margmodex+110,margmodey+110,100,50))
+            screen.blit(textbut2,(margmodex+120,margmodey+120))
         #Configuration for button 2
-        pygame.draw.rect(screen,RED,(margmodex+300,margmodey+110,70,50))
-        screen.blit(textbut3,(margmodex+310,margmodey+120))
-        if margmodex+300 < globmx < margmodex+370 and margmodey+120 < globmy < margmodey+170:
+        pygame.draw.rect(screen,RED,(margmodex+220,margmodey+110,70,50))
+        screen.blit(textbut3,(margmodex+230,margmodey+120))
+        if margmodex+220 < globmx < margmodex+290 and margmodey+120 < globmy < margmodey+170:
+            if clicked[0] == 1:
+                timerrun=False #Stop clock
+                seconds=0
+        #Configuration for button 3
+        pygame.draw.rect(screen,BLUE,(margmodex+300,margmodey+110,100,50))
+        screen.blit(textbut4,(margmodex+320,margmodey+120))
+        if margmodex+310 < globmx < margmodex+410 and margmodey+120 < globmy < margmodey+170:
             if clicked[0] == 1:
                 timerrun=False
+                #Reset clock
                 seconds=0
+                timer=0 
+                timerdisp=0
     else:
         screen.blit(textmanual1,(margmodex+160,margmodey+110))
         screen.blit(textmanual2,(margmodex+150,margmodey+140))
-    #Draw image objects in map
-    screen.blit(cursor,(arrowx,arrowy))
+    #Draw cursor in map
+    if boolcursor==True:
+        screen.blit(cursor,(arrowx,arrowy))
+    else:
+        screen.blit(cursor,(mxfixed-10,myfixed-20))
+    #Draw pointing circle in map
     screen.blit(circul,(coordx-30,coordy-30))
     #Draw text for angles insode circle
     screen.blit(textcircshad,(coordx-9,coordy+1))
